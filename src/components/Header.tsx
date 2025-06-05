f249f27e67c62c59d51095eee41c5ff87d6ee881
AiiiiDannn <xhai2@uci.edu>
2025-06-04 19:22:16 -0700

    Revision 0604: Modified color settings to align with uci color palette. Also new icons. Will finish post detail page and sending features (and so on) tomorrow.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,18 @@ const Header: React.FC<HeaderProps> = ({ onAddPost }) => {
   return (
     <header className="header">
       <div className="logo-title">
-        <h1>UCI TreeHole</h1>
+        <h1>ꪊᥴ꠸ ꪻ᥅ꫀꫀꫝꪮꪶꫀ</h1>
+        {/* 
+        GET FONT FROM https://tools.picsart.com/text/font-generator/
+        𝖀𝕮𝕴 𝕿𝖗𝖊𝖊𝕳𝖔𝖑𝖊
+        𝓤𝓒𝓘 𝓣𝓻𝓮𝓮𝓗𝓸𝓵𝓮
+        ꪊᥴ꠸ ꪻ᥅ꫀꫀꫝꪮꪶꫀ
+        𝕌ℂ𝕀 𝕋𝕣𝕖𝕖ℍ𝕠𝕝𝕖
+        𝔘ℭℑ 𝔗𝔯𝔢𝔢ℌ𝔬𝔩𝔢
+        𝒰𝒞𝐼 𝒯𝓇𝑒𝑒𝐻𝑜𝓁𝑒
+        𐌵𐌂𐌉 𐌕𐌓𐌄𐌄𐋅Ꝋ𐌋𐌄
+        ͓̽U͓͓̽̽C͓͓̽̽I͓̽ ͓̽T͓͓̽̽r͓͓̽̽e͓͓̽̽e͓͓̽̽H͓͓̽̽o͓͓̽̽l͓͓̽̽e͓̽
+         */}
         <div className="nav-buttons">
           <button className="tab active">🌿 New</button>
           <button className="tab">🔥 HOT</button>
@@ -18,11 +29,7 @@ const Header: React.FC<HeaderProps> = ({ onAddPost }) => {
       </div>
       <input type="text" className="search" placeholder="Search a post..." />
       <button className="add-post" onClick={onAddPost}>
-        <img
-          src="/create_new.svg"
-          alt="Create Post"
-          className="add-post-icon"
-        />
+        <img src="/new.svg" alt="Create Post" className="add-post-icon" />
       </button>
     </header>
   );
